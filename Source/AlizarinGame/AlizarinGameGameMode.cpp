f78dbe857629ef0d7e7145198601c699cc4ac869
mccannd <mccannd@seas.upenn.edu>
2016-09-14 17:01:00 -0400

    Initial commit

---
+++ Source/AlizarinGame/AlizarinGameGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AlizarinGame.h"
+#include "AlizarinGameGameMode.h"
+
+
+
+
---
+++ Source/AlizarinGame/AlizarinGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "AlizarinGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALIZARINGAME_API AAlizarinGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
